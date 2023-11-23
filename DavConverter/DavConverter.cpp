6375cef24ae25d6f67bbc55dd315aff82c7f309a
LLIEPLLIEHb <ismailov.96252445@gmail.com>
2023-11-23 23:36:06 +0300

    + code

---
+++ DavConverter/DavConverter.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+#include <fstream>
+#include <vector>
+
+
+bool ConvertDavToWav(const std::string& davFilePath) {
+    try {
+        // Открываем .dav файл для чтения в бинарном режиме
+        std::ifstream inputFile(davFilePath, std::ios::binary);
+        if (!inputFile.is_open()) {
+            fprintf(stderr, "Error: File does not exist or cannot be opened.\n");
+            return false;
+        }
+
+        // Получаем путь и имя файла без расширения
+        size_t lastDotIndex = davFilePath.find_last_of(".");
+        if (lastDotIndex == std::string::npos) {
+            fprintf(stderr, "Error: Invalid file name.\n");
+            return false;
+        }
+
+        std::string outputPath = davFilePath.substr(0, lastDotIndex) + ".wav";
+
+        // Читаем содержимое файла в байтовый вектор
+        std::vector<char> buffer((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
+
+        // Применяем преобразование XOR к каждому байту
+        for (char& byte : buffer) {
+            byte ^= 123;
+        }
+
+        // Закрываем входной файл
+        inputFile.close();
+
+        // Записываем преобразованные данные обратно в файл (.wav)
+        std::ofstream outputFile(outputPath, std::ios::binary);
+        if (!outputFile.is_open()) {
+            fprintf(stderr, "Error: Could not create or open output file.\n");
+            return false;
+        }
+
+        outputFile.write(buffer.data(), buffer.size());
+
+        // Закрываем выходной файл
+        outputFile.close();
+
+        printf("Файл сохранен: %s\n\n", outputPath.c_str());
+        return true;
+    }
+    catch (const std::exception& ex) {
+        fprintf(stderr, "Conversion failed. Reason: %s\n", ex.what());
+        return false;
+    }
+}
+
+int findDavFiles(const std::string& directory) {
+    int count = 0;
+    WIN32_FIND_DATAA findFileData;
+    HANDLE hFind = FindFirstFileA((directory + "\\*").c_str(), &findFileData);
+
+    if (hFind != INVALID_HANDLE_VALUE) {
+        do {
+            if (findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+                // Пропускаем . и ..
+                if (strcmp(findFileData.cFileName, ".") != 0 && strcmp(findFileData.cFileName, "..") != 0) {
+                    // Рекурсивно вызываем для подпапок
+                    count += findDavFiles(directory + "\\" + findFileData.cFileName);
+                }
+            }
+            else {
+                if (strstr(findFileData.cFileName, ".dav") != nullptr) {
+                    //printf("Найден файл: %s\n", (directory + "\\" + findFileData.cFileName).c_str());
+                    if (ConvertDavToWav(directory + "\\" + findFileData.cFileName)) {
+                        printf("Файл конвертируется в wav: %s\n", (directory + "\\" + findFileData.cFileName).c_str());
+                    }
+                    count++;
+                }
+            }
+        } while (FindNextFileA(hFind, &findFileData) != 0);
+        FindClose(hFind);
+    }
+
+    return count;
+}
+
+int main(int argc, char* argv[]) {
+    setlocale(LC_ALL, "Russian");
+    // Проверяем, передан ли аргумент командной строки
+    if (argc > 1) {
+        // Печатаем путь к папке
+        
+
+        // Вызываем функцию поиска файлов
+        int fileCount = findDavFiles(argv[1]);
+
+        // Печатаем суммарное количество найденных файлов
+        printf("\n\nСуммарное количество файлов .dav: %d\n", fileCount);
+        printf("Путь к папке: %s\n", argv[1]);
+    }
+    else {
+        // Если аргумента нет, сообщаем об этом
+        printf("Не указан путь к папке.\n");
+        return 1; // Возвращаем код ошибки
+    }
+
+    // Ожидаем ввода для того, чтобы консоль не закрылась сразу
+    printf("Нажмите Enter для завершения...\n");
+    std::string userInput;
+    std::getline(std::cin, userInput);
+
+    return 0; // Возвращаем успешный код завершения
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# DavtoWavConverter Train Simulator 2021
- Hi! I have created a small program for converting format files .wav in .wav, intended for use in the game Trainz Simulator 2021. I needed to extract encrypted sounds from the mod, and this program helped me with that. If you also need a similar solution, feel free to use it. However, please, dear developers, do not forget to specify my GitHub if you decide to use this code in your projects. Thank you for your attention!
+# DavtoWavConverter for Trainz Simulator 2021
+
+Hi there! I've developed a lightweight program for converting .dav files to .wav, designed for use in Trainz Simulator 2021. I encountered the need to extract encrypted sounds from a mod, and this program proved to be a handy solution. If you find yourself in a similar situation, feel free to use it. However, I kindly request fellow developers to credit my GitHub if you decide to incorporate this code into your projects. Thank you for your attention!
+
+## License
+
+MIT License
+
+© [Maksim Maltsev](https://vk.com/shagi_za_gigi) | Email: [shagi_za_gigi@vk.com]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS," WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE, AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
